757fbbee619d9f4e2b91caa905b6967bc79ac934
Alexander Georgiev <alexfromsofia@gmail.com>
2019-10-22 23:25:49 +0300

    Add main nav routes and prettier

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import Header from './Header';
 
-const App = () => <div>My react app</div>;
+const App = () => (
+  <div className="ui container">
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
